9a7f04d9a81d51f58c56df46c57eb91913c12226
SaSaChu <teresa7397@gmail.com>
2020-03-06 15:15:52 +0800

    社員資料修正

--- js/public.js
+++ js/public.js
@@ -1,9 +1,36 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 - 2019, Ginkgo
- * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://www.ioa.tw/
- */
+
  
 $(function() {
+
+    // 手機選單
+  $('.menu, .menu_boxs .icon-cross2').click(function() {
+    $('.menu_boxs').toggleClass ('hovermenu');
+    $('.cover').toggleClass('show');
+  });
+
+  $('.threeMenu').click(function() {
+    // $('.lastMenu_boxs').toggleClass ('showMenu');
+    $(this).next().toggleClass ('showMenu');
+  });
+
+  // 手機選單模式，背景資訊鎖定
+  function scrollLock () {
+    var scrollPosition = [
+      self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+      self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+    ];
+    var $html = $('html, body'); // it would make more sense to apply this to body, but IE7 won't have that
+    $html.data('scroll-position', scrollPosition);
+    $html.data('previous-overflow', $html.css('overflow'));
+    $html.css('overflow', 'hidden');
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+  }
+
+  function scrollUnlock () {
+    var $html = $('html, body'); // it would make more sense to apply this to body, but IE7 won't have that
+    var scrollPosition = $html.data('scroll-position');
+    $html.css('overflow', $html.data('previous-overflow'));
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+  }
+
 });
